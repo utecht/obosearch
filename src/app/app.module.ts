723205d46c9f3110f07ee4cbaf532ac71d4c7685
Joseph Utecht <thenmal@gmail.com>
2017-04-12 15:27:23 -0500

    initial search box styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdInputModule,
+         MdListModule,
+         MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdInputModule,
+    MdListModule,
+    MdCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
